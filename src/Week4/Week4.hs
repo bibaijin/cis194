56bf901ed9991cb6b2c0e2b4fa7b8e0992a703bc
kaizhang33 <kaizhang33@creditease.cn>
2016-11-27 14:51:56 +0800

    finished week4

--- src/Week4/Week4.hs
+++ src/Week4/Week4.hs
@@ -1,4 +1,22 @@
-module Week4.Week4 (fun1',fun2') where
+module Week4.Week4
+  (fun1
+  ,fun1'
+  ,fun2
+  ,fun2'
+  ,Tree(Leaf, Node)
+  ,foldTree
+  ,foldTree'
+  ,xor
+  ,map'
+  ,sieveSundaram)
+  where
+
+import Data.List
+
+main :: IO ()
+main = do
+  putStrLn $ show $ foldTree "ABCDEFGHIJ"
+  putStrLn $ show $ foldTree' "ABCDEFGHIJ"
 
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
@@ -17,11 +35,64 @@ fun2 n
 
 fun2' :: Integer -> Integer
 fun2' =
-    sum .
-    filter even .
-    takeWhile (/= 1) .
-    iterate
-        (\n ->
-              if even n
-                  then (n `div` 2)
-                  else (3 * n + 1))
+  sum .
+  filter even .
+  takeWhile (/= 1) .
+  iterate (\n ->
+             if even n
+                then (n `div` 2)
+                else (3 * n + 1))
+
+data Tree a
+  = Leaf
+  | Node Integer
+         (Tree a)
+         a
+         (Tree a)
+  deriving (Show,Eq)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insertTree Leaf
+
+insertTree :: a -> Tree a -> Tree a
+insertTree v Leaf = Node 0 Leaf v Leaf
+insertTree v (Node h l x r)
+  | getHeight l < getHeight r = Node h (insertTree v l) x r
+  | getHeight l == getHeight r = Node h' l' x r
+  | otherwise = Node h l x $ insertTree v r
+  where getHeight Leaf = -1
+        getHeight (Node h _ _ _) = h
+        l' = insertTree v l
+        h' = 1 + getHeight l'
+
+foldTree' :: [a] -> Tree a
+foldTree' [] = Leaf
+foldTree' xs =
+  Node height
+       (foldTree' $ take half xs)
+       (xs !! half)
+       (foldTree' $ drop (half + 1) xs)
+  where len = length xs
+        half = len `div` 2
+        height = floor (logBase 2 (fromIntegral len) :: Double)
+
+xor :: [Bool] -> Bool
+xor = foldr xor' False
+  where xor' False False = False
+        xor' False True = True
+        xor' True False = True
+        xor' True True = False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x, y) | x <- xs, y <- ys]
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map ((+ 1) . (* 2)) $ [1 .. n] \\ sieve
+  where sieve =
+          map (\(i,j) -> i + j + 2 * i * j) $
+          filter (\(i,j) -> i <= j && (i + j + 2 * i * j <= n)) $
+          cartProd [1 .. n]
+                   [1 .. n]
--- test/Week4/Week4Spec.hs
+++ test/Week4/Week4Spec.hs
@@ -3,11 +3,62 @@ module Week4.Week4Spec where
 import Test.Hspec
 import Test.QuickCheck
 
+import Week4.Week4
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  describe "fun1'" $ do
-    it "property" $ do
-      property $ \xs -> fun1' xs == fun1 (xs :: [Integer])
+spec =
+  do describe "fun1'" $
+       do it "property" $
+            do property $ \xs -> fun1' xs == fun1 (xs :: [Integer])
+     describe "fun2'" $
+       do it "cases" $
+            do fun2' 1 `shouldBe` fun2 1
+               fun2' 10 `shouldBe` fun2 10
+               fun2' 82 `shouldBe` fun2 82
+               fun2' 113 `shouldBe` fun2 113
+     describe "foldTree" $
+       do it "cases" $
+            do foldTree [] `shouldBe` (Leaf :: Tree Char)
+               foldTree "A" `shouldBe` Node 0 Leaf 'A' Leaf
+               foldTree "ABCDEFGHIJ" `shouldBe`
+                 Node 3
+                      (Node 2
+                            (Node 1 (Node 0 Leaf 'D' Leaf) 'G' Leaf)
+                            'I'
+                            (Node 1 (Node 0 Leaf 'A' Leaf) 'E' Leaf))
+                      'J'
+                      (Node 2
+                            (Node 1 (Node 0 Leaf 'B' Leaf) 'F' Leaf)
+                            'H'
+                            (Node 0 Leaf 'C' Leaf))
+     describe "foldTree'" $
+       do it "cases" $
+            do foldTree' [] `shouldBe` (Leaf :: Tree Char)
+               foldTree' "A" `shouldBe` Node 0 Leaf 'A' Leaf
+               foldTree' "ABCDEFGHIJ" `shouldBe`
+                 Node 3
+                      (Node 2
+                            (Node 1 (Node 0 Leaf 'A' Leaf) 'B' Leaf)
+                            'C'
+                            (Node 1 (Node 0 Leaf 'D' Leaf) 'E' Leaf))
+                      'F'
+                      (Node 2
+                            (Node 1 (Node 0 Leaf 'G' Leaf) 'H' Leaf)
+                            'I'
+                            (Node 0 Leaf 'J' Leaf))
+     describe "xor" $
+       do it "cases" $
+            do xor [False,True,False] `shouldBe` True
+               xor [False,True,False,False,True] `shouldBe` False
+          it "property" $
+            do property $
+                 \xs ->
+                   xor xs ==
+                   if even (length $
+                            filter (\x -> x)
+                                   (xs :: [Bool]))
+                      then False
+                      else True
